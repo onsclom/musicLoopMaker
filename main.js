ff5eae46cbc547d7f1a98dafa08d86cdd9f41e11
Austin Merrick <austinmerrick98@gmail.com>
2020-03-19 22:36:00 -0700

    Adding abilility to click and hold.

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-var checkboxes = [...document.querySelectorAll(".checkboxMain"), ...document.querySelectorAll(".checkbox")];
+let checkboxes = [...document.querySelectorAll(".checkboxMain"), ...document.querySelectorAll(".checkbox")];
+
+let mouseIsDown = false;
+
+document.onmousedown = docOnMousedown;
+document.onmouseup = docOnMouseup;
+function docOnMousedown(e)
+{
+  mouseIsDown = true;
+}
+function docOnMouseup(e)
+{
+  mouseIsDown = false;
+}
 
 checkboxes.forEach(function(curBox) {
-  curBox.addEventListener("click", toggle);
+  curBox.addEventListener("mousedown", drumClicked);
+  curBox.addEventListener("mouseover", drumHovered);
 });
 
-function toggle(clicked) {
+function drumHovered(clicked) {
+    if (mouseIsDown)
+    {
+        if (clicked.target.classList.contains('checkboxSelected'))
+        {
+            clicked.target.classList.remove("checkboxSelected");
+        }
+        else
+        {
+            clicked.target.classList.add("checkboxSelected");;
+        }
+    }
+}
+
+function drumClicked(clicked) {
     if (clicked.target.classList.contains('checkboxSelected'))
     {
         clicked.target.classList.remove("checkboxSelected");
