c703863a293b185a0c8b51510bd2114ebc90fd17
Austin Merrick <austinmerrick98@gmail.com>
2020-03-19 23:51:55 -0700

    Adding play button and note storing.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 let checkboxes = [...document.querySelectorAll(".checkboxMain"), ...document.querySelectorAll(".checkbox")];
 
+kickRow = [];
+snareRow = [];
+closedRow = [];
+openRow = [];
+
+for (let row of [kickRow, snareRow, closedRow, openRow]) {
+    for (let i = 0; i<16; i++)
+    {
+        row.push(false);
+    }
+}
+
 let mouseIsDown = false;
 
 document.onmousedown = docOnMousedown;
@@ -13,21 +25,21 @@ function docOnMouseup(e)
   mouseIsDown = false;
 }
 
-checkboxes.forEach(function(curBox) {
+checkboxes.forEach((curBox) => {
   curBox.addEventListener("mousedown", drumClicked);
   curBox.addEventListener("mouseover", drumHovered);
 });
 
-function drumHovered(clicked) {
+function drumHovered(hovered) {
     if (mouseIsDown)
     {
-        if (clicked.target.classList.contains('checkboxSelected'))
+        if (hovered.target.classList.contains('checkboxSelected'))
         {
-            clicked.target.classList.remove("checkboxSelected");
+            removeBeat(hovered.target);
         }
         else
         {
-            clicked.target.classList.add("checkboxSelected");;
+            addBeat(hovered.target);
         }
     }
 }
@@ -35,10 +47,67 @@ function drumHovered(clicked) {
 function drumClicked(clicked) {
     if (clicked.target.classList.contains('checkboxSelected'))
     {
-        clicked.target.classList.remove("checkboxSelected");
+        removeBeat(clicked.target);
     }
     else
     {
-        clicked.target.classList.add("checkboxSelected");;
+        addBeat(clicked.target);
     }
+}
+
+function addBeat(element) {
+    element.classList.add("checkboxSelected");
+    let row = element.parentNode.id;
+    let beat = element.id;
+
+    if (row == "kickRow")
+    {
+        kickRow[beat-1] = true;
+    }
+    else if (row == "snareRow")
+    {
+        snareRow[beat-1] = true;
+    }
+    else if (row == "closedRow")
+    {
+        snareRow[beat-1] = true;
+    }
+    else if (row == "openRow")
+    {
+        snareRow[beat-1] = true;
+    }
+}
+
+function removeBeat(element) {
+    element.classList.remove("checkboxSelected");
+    let row = element.parentNode.id;
+    let beat = element.id;
+    
+    if (row == "kickRow")
+    {
+        kickRow[beat-1] = false;
+    }
+    else if (row == "snareRow")
+    {
+        snareRow[beat-1] = false;
+    }
+    else if (row == "closedRow")
+    {
+        snareRow[beat-1] = false;
+    }
+    else if (row == "openRow")
+    {
+        snareRow[beat-1] = false;
+    }
+}
+
+let playing = false;
+function togglePlay(element) {
+    if (playing) {
+        element.src = "svgs/play.svg";
+    }
+    else {
+        element.src = "svgs/pause.svg";
+    }
+    playing = !playing;
 }
